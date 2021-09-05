46f8f11cc2775df3bfd607a736606350b6c4c57c
KarbonKitty <308lapua.magnum@gmail.com>
2021-09-05 15:32:25 +0200

    Display prices using thousands separator

--- Backend/DisplayHelpers.cs
+++ Backend/DisplayHelpers.cs
@@ -1,4 +1,4 @@
 public static class DisplayHelpers
 {
-    public static string DisplayPrice(double value) => $"{value:F2}$";
+    public static string DisplayPrice(double value) => $"{value:N2}$";
 }
